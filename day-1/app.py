39fff7f3ee86d9102116aa7161a3217a6caaace2
Tyler <hachadorian98@gmail.com>
2022-12-05 13:52:31 -0500

    forgot to do part 2 in day 1

--- day-1/app.py
+++ day-1/app.py
@@ -18,3 +18,19 @@ with open('./input.txt') as file:
             new_elf = Elf()
 
 print(highest)
+
+# part 2 probably a better way to do it but lazy
+
+elves = []
+
+with open('./input.txt') as file:
+    new_elf = Elf()
+    for line in file.readlines():
+        if len(line) != 1:
+            new_elf.items.append(int(line))
+        else:
+            elves.append(new_elf.total())        
+            new_elf = Elf()
+
+elves.sort(reverse=True)
+print(sum(elves[:3]))
